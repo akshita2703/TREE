4fbfaf67a3d200dcccb76b6ea637ef6cc48c1190
akshita2703 <rastogi32akshita@gmail.com>
2024-05-18 12:34:55 +0530

    tree

--- Love_Babbar/00_Lec1/00_implementation.cpp
+++ Love_Babbar/00_Lec1/00_implementation.cpp
@@ -37,11 +37,41 @@ Node* createTree(){
 
     return root;
 }
+void preOrder(Node* root){
+    if(!root) return ;
+    cout<<root->data<<" ";
+    preOrder(root->left);
+    preOrder(root->right);
 
+}
+void inOrder(Node* root){
+    if(!root) return ;
+   
+    inOrder(root->left);
+     cout<<root->data<<" ";
+    inOrder(root->right);
+
+}
+void postOrder(Node* root){
+    if(!root) return ;
+    
+    postOrder(root->left);
+    postOrder(root->right);
+    cout<<root->data<<" ";
+
+}
+// complexity of these 3 traversal are 
+// O(n) time and O(h) space h is the height of node (incase of skewtree it will be O(n-- number of nodes))
 int main(){
 
     Node* root = createTree();
     cout << root->data << endl;
+    preOrder(root);
+    cout<<endl;
+    postOrder(root);
+    cout<<endl;
+
+    inOrder(root);
 
     return 0;
 
