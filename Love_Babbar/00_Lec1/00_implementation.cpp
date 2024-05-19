08116868dfdc701c7276b99035dccf21d388ac1c
akshita2703 <rastogi32akshita@gmail.com>
2024-05-19 18:46:58 +0530

    create tree from inorder and preOrder

--- Love_Babbar/00_Lec1/00_implementation.cpp
+++ Love_Babbar/00_Lec1/00_implementation.cpp
@@ -102,20 +102,69 @@ int maxDepth(Node * root){
     return 1+max(left,right);
 }
 
+int searchIdx(int inorder[],int size, int element){
+    for(int i =0;i<size;i++){
+        if(inorder[i]==element){
+            return i;
+        }
+    }
+    return -1;
+}
+
+Node* constructTree(int preorder[], int &preIndx , int inorder[],int inStart,int inEnd,int size){
+    // base case 
+    if(preIndx>=size || inStart>inEnd) return NULL;
+    // 1 case solve krte h
+    int element = preorder[preIndx];
+    preIndx++;
+    Node* root = new Node(element);
+    // element search krte h 
+    int position = searchIdx(inorder,size,element);
+    // bki case recursion smbhl lega
+    root->left = constructTree(preorder,preIndx,inorder,inStart,position-1,size);
+    root->right = constructTree(preorder,preIndx,inorder,position+1,inEnd,size);
+    return root;
+}
+
+
 int main(){
+    int inorder[] = {10,8,6,2,4,12};
+    int preorder[] = {2,8,10,6,4,12};
+    int preIndx = 0;
+    int inStart = 0;
+    int inEnd = 5;
+    int size = 6;
+    Node* root =  constructTree(preorder,preIndx,inorder,inStart,inEnd,size);
+    cout<< "printing entire tree"<<endl;
+    levelOrder(root);
+//  2
+// 8 8
+// 10 10 10
+// 6 6
+// 4 4 4           ye glt output h kyuki mene preIndex by refernece nhi bhjaa
+// 12 12 12 12
 
-    Node* root = createTree();
-    cout << root->data << endl;
-    preOrder(root);
-    cout<<endl;
-    postOrder(root);
-    cout<<endl;
-    inOrder(root);
-    cout<<endl;
-    cout<<levelOrder(root);
-    cout<<endl;
+// 2          ye sahi output h boss
+// 8 4
+// 10 6 12
+    
 
-    cout<<maxDepth(root)<<endl;
+    // Node* root = createTree();
+    // cout << root->data << endl;
+    // preOrder(root);
+    // cout<<endl;
+    // postOrder(root);
+    // cout<<endl;
+    // inOrder(root);
+    // cout<<endl;
+    // cout<<levelOrder(root);
+    // cout<<endl;
+
+    // cout<<maxDepth(root)<<endl;
+
+
+   
+
 
 
     return 0;
